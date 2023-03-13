b769dc8e7bd52469b848c65c58b4c2d1e4bef18e
RakibHossainMorul <67518360+RakibHossainMorul@users.noreply.github.com>
2023-03-14 00:35:07 +0600

    added

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Vegetables Shop Application',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: 
     );
   }
 }
